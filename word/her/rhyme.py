4de9c46a8b2fd27dca67d7ec014eb85738a94291
Arthur <arthur.j.johnston@gmail.com>
2014-09-30 17:59:19 -0400

    pretty print

--- word/her/rhyme.py
+++ word/her/rhyme.py
@@ -1,6 +1,8 @@
 from nltk.corpus import cmudict
+#pro is pronunciation
+
 #realRhymes=dict((k,v) for k,v in rhymeToPro.items() if len(v)>1)
-def getPronounciationGroupedByRhyme(wordToPro=None):
+def getDictionariesNeededForRhyming(wordToPro=None):
 	if wordToPro is None:
 		wordToPro=cmudict.dict();
 	rhymeToPro=dict()
@@ -10,10 +12,10 @@ def getPronounciationGroupedByRhyme(wordToPro=None):
 				pronunciation=pr[:]
 				pronunciation.reverse()
 				p=getUntilStressed(pronunciation)
-				if tuple(p) in rhymeToPro and rhymeToPro[tuple(p)] is not None: 
-					rhymeToPro[tuple(p)].add(tuple(pronunciation))
+				if p in rhymeToPro and rhymeToPro[p] is not None: 
+					rhymeToPro[p].add(tuple(pronunciation))
 				else:
-					rhymeToPro[tuple(p)]={tuple(pronunciation)}
+					rhymeToPro[p]={tuple(pronunciation)}
 
 				if tuple(pronunciation) in pronunciationToWord and pronunciationToWord[tuple(pronunciation)] is not None:
 					pronunciationToWord[tuple(pronunciation)].append(key)
@@ -21,11 +23,21 @@ def getPronounciationGroupedByRhyme(wordToPro=None):
 					pronunciationToWord[tuple(pronunciation)]=[key]
 	return rhymeToPro,pronunciationToWord;
 
-def getUntilStressed(pr):
+def getUntilStressed(phonemes):
 	result=list();
-	for p in pr:
+	for p in phonemes:
 		result.append(p)
 		if(p[-1]=='1'):
 			break;
-	return result;
+	return tuple(result);
 
+def printRhymingWords(word,wordToPros,proToWords,rhymeToPros):
+	print('The word is '+word)
+	pros=wordToPros[word]
+	for pro in pros:
+		rPro=pro[:]
+		rPro.reverse()
+		rhymeGroup=getUntilStressed(rPro)
+		print('rhyme group'+str(rhymeGroup))
+		for v in rhymeToPros[rhymeGroup]:
+			print('  '+str(proToWords[v]))
