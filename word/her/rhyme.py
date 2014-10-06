89138bfe4056b340f804f8925afdba01323deba4
Arthur <arthur.j.johnston@gmail.com>
2014-10-06 08:25:30 -0400

    added a class for the properties needed

--- word/her/rhyme.py
+++ word/her/rhyme.py
@@ -1,7 +1,12 @@
 from nltk.corpus import cmudict
 #pro is pronunciation
-
+#import imp
+#rhyme=imp.load_source('rhyme','C:/Sites/blanker/word/her/rhyme.py')
+#from nltk.corpus import cmudict
+#cmu=cmudict.dict()
+#rhymeToPro,pronunciationToWord=rhyme.getDictionariesNeededForRhyming(cmu)
 #realRhymes=dict((k,v) for k,v in rhymeToPro.items() if len(v)>1)
+
 def getDictionariesNeededForRhyming(wordToPro=None):
 	if wordToPro is None:
 		wordToPro=cmudict.dict();
@@ -43,7 +48,17 @@ def printRhymingWords(word,wordToPros,proToWords,rhymeToPros,identity=True):
 		rPro=pro[:]
 		rPro.reverse()
 		rhymeGroup=getUntilStressed(rPro)
-		print('rhyme group'+str(rhymeGroup[::-1]))
+		print('rhyme group'+str(rhymeGroup[::-1])) #reversed to make it easier to read
 		for pro in rhymeToPros[rhymeGroup]:
+			if(any (x[0]!=x[1] for x in tuple(zip(pro,rPro)))): #non identity
+				print('  '+str(proToWords[pro]))
+			else:
+				print('identity words '+str(proToWords[pro]))
 
-			print('  '+str(proToWords[pro]))
+class rhymeGroup:
+	def __init__(self,rhyme,rhymeToPros,proToWords):
+		self.rhyme=rhyme
+		self.proToWords=dict()
+		for pro in rhymeToPros[rhyme]:
+			self.proToWords[pro]=proToWords[pro]
+
